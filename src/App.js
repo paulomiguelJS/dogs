81231a9552505bc12653ae795840cd5251ed1045
paulomiguelJS <paulomiguelblues@gmail.com>
2021-11-05 15:02:25 -0400

    Header and Footer created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React from 'react';
 import './App.css';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
