09940cb7c889e5b8eb2c1c298b5f6ec5affb8624
paulomiguelJS <paulomiguelblues@gmail.com>
2021-11-07 15:56:00 -0500

    Header and Routes implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
-import Header from './Components/Header/Header';
-import Footer from './Components/Footer/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header/index';
+import Footer from './Components/Footer/index';
+import Home from './Components/Home/index';
+import Login from './Components/Login/index';
 
 function App() {
   return (
     <div>
-      <Header />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
