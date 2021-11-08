e4bd424b519a398bfc7172e3452936ffc19fe1d0
paulomiguelJS <paulomiguelblues@gmail.com>
2021-11-07 22:20:19 -0500

    LoginPage and * creted / API

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LoginForm from './LoginForm';
+import LoginCreate from './LoginCreate.js';
+import LoginPasswordLost from './LoginPasswordLost';
+import LoginPasswordReset from './LoginPasswordReset';
 
-function Login() {
-  return <div>Login</div>;
-}
+const index = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="criar" element={<LoginCreate />} />
+        <Route path="perdeu" element={<LoginPasswordLost />} />
+        <Route path="resetar" element={<LoginPasswordReset />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default Login;
+export default index;
