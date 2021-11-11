48736a7349ce32261de9da8f66414e882e84b2fa
paulomiguelJS <paulomiguelblues@gmail.com>
2021-11-11 00:11:11 -0500

    Adding UserHeader, photo, stats, modules

--- src/Components/User/index.js
+++ src/Components/User/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import UserHeader from './UserHeader';
+import { Routes, Route } from 'react-router-dom';
+import Feed from '../Feed/index';
+import UserPhotoPost from './UserPhotoPost';
+import UserStats from './UserStats';
 
 const User = () => {
-  return <div></div>;
+  return (
+    <section className="container">
+      <UserHeader />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="postar" element={<UserPhotoPost />} />
+        <Route path="estatisticas" element={<UserStats />} />
+      </Routes>
+    </section>
+  );
 };
 
 export default User;
