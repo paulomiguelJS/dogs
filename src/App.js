f14dc2c50d0f23e96e973fec561fba9299e3e84f
paulomiguelJS <paulomiguelblues@gmail.com>
2021-11-10 23:34:00 -0500

    Adding ProtectedRoute and User

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Footer from './Components/Footer/index';
 import Home from './Components/Home/index';
 import Login from './Components/Login/index';
 import { UserStorage } from './UserContext';
+import User from './Components/User';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
 
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login/*" element={<Login />} />
+            <ProtectedRoute path="/conta/*" element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>
--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -45,3 +45,5 @@ const LoginCreate = () => {
 };
 
 export default LoginCreate;
+
+
